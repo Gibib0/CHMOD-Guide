d55db0b46cffe9b3d1029e638692fb1d144b7db2
Anastasia <anast111antonova@gmail.com>
2025-08-21 18:06:32 +0300

    Add switch-case construction with default check for correctness of the entered number

--- src/index.js
+++ src/index.js
@@ -4,4 +4,33 @@ let inputNum = Number(prompt('Enter a number from 0 to 7: '))
 
 if (Number.isNaN(inputNum)) {
 	console.log('Entered wrong data');
+} else {
+	switch (inputNum) {
+		case 0:
+			console.log('Level of rights: ---');
+			break
+		case 1:
+			console.log('Level of rights: --x');
+			break
+		case 2:
+			console.log('Level of rights: -w-');
+			break
+		case 3:
+			console.log('Level of rights: -wx');
+			break
+		case 4:
+			console.log('Level of rights: r--');
+			break
+		case 5:
+			console.log('Level of rights: r-x');
+			break
+		case 6:
+			console.log('Level of rights: rw-');
+			break
+		case 7:
+			console.log('Level of rights: rwx');
+			break
+		default:
+			console.log('Entered wrong number. Enter a number from 0 to 7');
+	}
 }
